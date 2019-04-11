9569296d235de16b9ac724ba39d3f0190acd833e
juanyanza11 <ww4fjj@gmail.com>
2019-04-11 12:05:26 -0500

    Subiendo Ejercicio en clase

--- ejemplosenclase2/src/EjercicioClase/Mieejercicio.java
+++ ejemplosenclase2/src/EjercicioClase/Mieejercicio.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejemplosenclase2;
+package EjercicioClase;
 
 import java.util.Scanner;
 
